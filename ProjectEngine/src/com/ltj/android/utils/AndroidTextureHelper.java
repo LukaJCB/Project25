81faaefb683cda19c00387f73d45e7fb4a4f3708
LukaJCB <luka.jacobowitz@fh-duesseldorf.de>
2015-05-17 17:05:44 +0200

    Added ability to load textures from path

--- ProjectEngine/src/com/ltj/android/utils/AndroidTextureHelper.java
+++ ProjectEngine/src/com/ltj/android/utils/AndroidTextureHelper.java
@@ -55,4 +55,39 @@ public class AndroidTextureHelper {
 		
 		
 	}
+	
+	public static int loadTexture(String path){
+		final int[] textureHandle = new int[1];
+		 
+	    glGenTextures(1, textureHandle, 0);
+	 
+	    if (textureHandle[0] != 0)
+	    {
+	        final BitmapFactory.Options options = new BitmapFactory.Options();
+	        options.inScaled = false;   // No pre-scaling
+	 
+	        // Read in the resource
+	        Bitmap bitmap = BitmapFactory.decodeFile(path, options);
+	        
+	        // Bind to the texture in OpenGL
+	        glBindTexture(GL_TEXTURE_2D, textureHandle[0]);
+	 
+	        // Set filtering
+	        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	 
+	        // Load the bitmap into the bound texture.
+	        GLUtils.texImage2D(GL_TEXTURE_2D, 0, bitmap, 0);
+	 
+	        // Recycle the bitmap, since its data has been loaded into OpenGL.
+	        bitmap.recycle();
+	    }
+	 
+	    if (textureHandle[0] == 0)
+	    {
+	        throw new RuntimeException("Error loading texture.");
+	    }
+	 
+	    return textureHandle[0];
+	}
 }
