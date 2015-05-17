c9d7763c0726d4be2c8d85d5c0470bf408bc8529
LukaJCB <luka.jacobowitz@fh-duesseldorf.de>
2015-05-17 10:24:57 +0200

    Split RenderObject into render and gameobject

---
+++ ProjectEngine/src/com/ltj/shared/engine/GameObject.java
@@ -0,0 +1,20 @@
+package com.ltj.shared.engine;
+
+public interface GameObject {
+	void destroy();
+	void translate(float dx,float dy);
+	void translate(float dx, float dy, float width, float height);
+	void scale(float sx,float sy);
+	void rotate(float deg);
+	float getX();
+	float getY();
+	float getHeight();
+	float getWidth();
+	void setTag(String tag);
+	boolean compareTag(String string);
+	float getRotation();
+	boolean collidesWith(RenderObject collider);
+	boolean isDestroyed();
+	GameObject getParent();
+	Behaviour<? extends GameObject> getBehaviour();
+}
--- ProjectEngine/src/com/ltj/shared/engine/RenderObject.java
+++ ProjectEngine/src/com/ltj/shared/engine/RenderObject.java
@@ -4,31 +4,17 @@ package com.ltj.shared.engine;
 import java.util.ArrayList;
 
 
-public interface RenderObject {
+public interface RenderObject extends GameObject {
 	void start();
 	void update();
 	void render();
-	void destroy();
-	void translate(float dx,float dy);
-	void translate(float dx, float dy, float width, float height);
-	void scale(float sx,float sy);
-	void rotate(float deg);
-	float getX();
-	float getY();
-	float getHeight();
-	float getWidth();
-	boolean collidesWith(RenderObject collider);
-	boolean isDestroyed();
-	void addBehaviour(Behaviour<? extends RenderObject> b);
+	void addBehaviour(Behaviour<? extends GameObject> b);
 	void addCollider(Collider c);
 	Collider getCollider(int index);
 	ArrayList<Collider> getColliders();
-	void onCollision(RenderObject collider);
-	void onChildCollision(RenderObject child, RenderObject collider);
-	void setTag(String tag);
-	boolean compareTag(String string);
-	float getRotation();
+	void onCollision(GameObject collider);
+	void onChildCollision(GameObject child, GameObject collider);
 	void addChild(RenderObject child);
-	RenderObject getParent();
+	
 	
 }
