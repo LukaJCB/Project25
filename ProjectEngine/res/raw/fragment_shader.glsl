06af773801df5e11642de66a5a769c2f2ff5bba7
LukaJCB <luka.jacobowitz@fh-duesseldorf.de>
2015-05-25 23:42:49 +0200

    Revert "Revert "Revert "Revert "test""""
    This reverts commit 80433f87f87f39d75459be566c1e70f23a15f2a1.

--- ProjectEngine/res/raw/fragment_shader.glsl
+++ ProjectEngine/res/raw/fragment_shader.glsl
@@ -5,5 +5,5 @@ uniform sampler2D uTexture;
 varying vec2 vTexCoordinate;
 
 void main(){
-	gl_FragColor = texture2D(uTexture, vTexCoordinate);
+	gl_FragColor = vec4(1.0,0.0,0.0,0.0)*texture2D(uTexture, vTexCoordinate);
 }
