892ca1b03bb01ff716b3c9de4ae27b6ee85c9403
LukaJCB <luka.jacobowitz@fh-duesseldorf.de>
2015-06-20 13:35:00 +0200

    Reworked SpatialHashMap

--- ProjectEngine/src/com/ltj/shared/engine/Updater.java
+++ ProjectEngine/src/com/ltj/shared/engine/Updater.java
@@ -89,27 +89,8 @@ public abstract class Updater {
 			shMap.insert(r);
 		}
 		shMap.collideAll();
-//		qTree.clear();
-//		for (RenderObject r: allObjects){
-//			qTree.insert(r);
-//		}
-//		qTree.collideAll();
-		
-		
-//		List<RenderObject> returnObjects = new ArrayList<RenderObject>();
-//		for (int i = 0; i < allObjects.size(); i++) {
-//		  returnObjects.clear();
-//		  qTree.retrieve(returnObjects, allObjects.get(i));
-//		 
-//		  for (int x = 0; x < returnObjects.size(); x++) {
-//			  allObjects.get(i).checkCollision(allObjects.get(x));
-//		  }
-//		}
-		
-		
 //		for (int i = 0;i < allObjects.size(); i++){
 //			for (int j = i+1; j < allObjects.size(); j++){
-//				
 //				allObjects.get(i).checkCollision(allObjects.get(j));
 //			}
 //		}
--- ProjectEngine/src/com/ltj/shared/engine/primitives/SpatialHashMap.java
+++ ProjectEngine/src/com/ltj/shared/engine/primitives/SpatialHashMap.java
@@ -7,51 +7,51 @@ import com.ltj.shared.engine.RenderObject;
 
 public class SpatialHashMap {
 
-	private ArrayList<RenderObject>[] map;
-	private int columns;
+	private ArrayList<RenderObject>[][] map;
 	private float cellHeight, cellWidth;
 	private float gridX, gridY;
 	
 	
 	@SuppressWarnings("unchecked")
 	public SpatialHashMap(int cols, int rows, float width, float height, float gridX,float gridY){
-		this.columns = cols;
 		this.gridX = gridX - width/2;
 		this.gridY = gridY - height/2;
 		cellHeight = height/rows;
 		cellWidth = width/cols;
-		map = new ArrayList[cols*rows];
-		for (int i = 0;i < cols * rows;i++){
-			map[i] = new ArrayList<RenderObject>();
+		map = new ArrayList[cols][rows];
+		for (int i = 0;i < cols;i++){
+			for (int j = 0; j < rows;j++){
+				map[i][j] = new ArrayList<RenderObject>();
+			}
 		}
 	}
 	public void insert(RenderObject sprite){
 		if (sprite.getColliders() != null){
 			for (Collider c : sprite.getColliders()){
-				int bottom = (int) ((c.getBottom(sprite.getY(), sprite.getHeight())-gridY)/cellHeight);
-				int top = (int) ((c.getTop(sprite.getY(), sprite.getHeight())-gridY)/cellHeight);
+				int bottom = clampY((int) ((c.getBottom(sprite.getY(), sprite.getHeight())-gridY)/cellHeight));
+				int top = clampY((int) ((c.getTop(sprite.getY(), sprite.getHeight())-gridY)/cellHeight));
 				boolean topBot = (top == bottom); 
 
-				int left = (int) ((c.getLeft(sprite.getX(), sprite.getWidth())-gridX)/cellWidth);
-				int right = (int) ((c.getRight(sprite.getX(), sprite.getWidth())-gridX)/cellWidth);	
+				int left = clampX((int) ((c.getLeft(sprite.getX(), sprite.getWidth())-gridX)/cellWidth));
+				int right = clampX((int) ((c.getRight(sprite.getX(), sprite.getWidth())-gridX)/cellWidth));	
 				boolean rightLeft = (right == left); 
 
 				if (rightLeft){
 					if (topBot){
-						map[getIdInMap(left,bottom)].add(sprite);
+						map[left][bottom].add(sprite);
 					} else {
-						map[getIdInMap(left, bottom)].add(sprite);
-						map[getIdInMap(left, top)].add(sprite);
+						map[left][bottom].add(sprite);
+						map[left][top].add(sprite);
 					}
 				} else {
 					if (topBot){
-						map[getIdInMap(left,bottom)].add(sprite);
-						map[getIdInMap(right, bottom)].add(sprite);
+						map[left][bottom].add(sprite);
+						map[right][bottom].add(sprite);
 					} else {
-						map[getIdInMap(left, bottom)].add(sprite);
-						map[getIdInMap(right, bottom)].add(sprite);
-						map[getIdInMap(left, top)].add(sprite);
-						map[getIdInMap(right, top)].add(sprite);
+						map[left][bottom].add(sprite);
+						map[right][bottom].add(sprite);
+						map[left][top].add(sprite);
+						map[right][top].add(sprite);
 					}
 				}
 			}
@@ -59,23 +59,44 @@ public class SpatialHashMap {
 	}
 	
 	
-	private int getIdInMap(int x, int y){
-		return y * columns + x;
+	
+	private int clampX(int value){
+		if (value < 0){
+			return 0;
+		}
+		if (value >= map.length){
+			return map.length-1;
+		}
+		return value;
+	}
+	
+	private int clampY(int value){
+		if (value < 0){
+			return 0;
+		}
+		if (value >= map[0].length){
+			return map[0].length-1;
+		}
+		return value;
 	}
 	
 	public void collideAll(){
-		for (ArrayList<RenderObject> allObjects : map){
-			for (int i = 0;i < allObjects.size(); i++){
-				for (int j = i+1; j < allObjects.size(); j++){
-					allObjects.get(i).checkCollision(allObjects.get(j));
+		for (ArrayList<RenderObject>[] allArrays : map){
+			for(ArrayList<RenderObject> allObjects : allArrays){
+				for (int i = 0;i < allObjects.size(); i++){
+					for (int j = i+1; j < allObjects.size(); j++){
+						allObjects.get(i).checkCollision(allObjects.get(j));
+					}
 				}
 			}
 		}
 	}
 	
 	public void clear() {
-		for (ArrayList<RenderObject> allObjects : map){
-			allObjects.clear();
+		for (ArrayList<RenderObject>[] allArrays : map){
+			for(ArrayList<RenderObject> allObjects : allArrays){
+				allObjects.clear();
+			}
 		}
 		
 	}
