6e739c5bb57f9aa017d7bff30c08a073ed3a9708
LukaJCB <luka.jacobowitz@fh-duesseldorf.de>
2015-07-08 00:01:15 +0200

    added gameobjects to globals

--- ProjectEngine/src/com/ltj/shared/engine/primitives/AnimationPlayer.java
+++
@@ -1,7 +0,0 @@
-package com.ltj.shared.engine.primitives;
-
-public class AnimationPlayer {
-	public static void play(SimpleAnimation anim){
-		
-	}
-}
--- ProjectEngine/src/com/ltj/shared/engine/primitives/Globals.java
+++ ProjectEngine/src/com/ltj/shared/engine/primitives/Globals.java
@@ -2,12 +2,15 @@ package com.ltj.shared.engine.primitives;
 
 import java.util.HashMap;
 
+import com.ltj.shared.engine.GameObject;
+
 public class Globals {
 	private Globals(){}
 
 	private static HashMap<String,Boolean> boolMap = new HashMap<String,Boolean>();
 	private static HashMap<String,Float> floatMap = new HashMap<String,Float>();
 	private static HashMap<String,Integer> intMap = new HashMap<String,Integer>();
+	private static HashMap<String,GameObject> objectMap = new HashMap<String,GameObject>();
 	
 	public static void add(String key, boolean value){
 		boolMap.put(key, value);
@@ -18,16 +21,22 @@ public class Globals {
 	public static void add(String key, int value){
 		intMap.put(key, value);
 	}
+	
+	public static void add(String key, GameObject value){
+		objectMap.put(key, value);
+	}
 	public static boolean getBool(String key){
-		return boolMap.get(key);
+		return boolMap.getOrDefault(key,false);
 	}
 	public static float getFloat(String key){
-		if (floatMap.get(key) != null){
-			return floatMap.get(key);
-		}
-		return 0;
+		return floatMap.getOrDefault(key, 0.0f);
 	}
-	public static float getInt(String key){
-		return intMap.get(key);
+	public static int getInt(String key){
+		return intMap.getOrDefault(key,0);
 	}
+	
+	public static GameObject getGameObject(String key){
+		return objectMap.get(key);
+	}
+
 }
